5f768ccf311c4f73a86d2e9c67944b1016d507a2
Nima sherpa <73970332+NimaSherpa10@users.noreply.github.com>
2024-09-03 08:54:18 -0700

    swap two numbers without temp

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
 public static void main(String[] args) {
-    System.out.println("Hello");
+     int a = 20;
+     int b = 30;
+     System.out.println(a + "\t" + b);
+     int x = b;
+      b = a;
+      a = x;
+      System.out.println(a + "\t" + b);
     }
 }
---
+++ swapNumbers.java
@@ -0,0 +1,16 @@
+public class swapNumbers {
+    //swapping without temp variables
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        
+        System.out.println("Before swapping");
+        System.out.println(a + "\t" + b);
+
+         a = a + b; //20 + 30 : 50
+         b = a - b; // 50 - 30 = 20
+         a = a - b; 
+         System.out.println("After swapping");
+         System.out.println(a + "\t" + b);
+    }
+}
