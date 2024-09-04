91bf762764bed1d8cde6c7dea19bcbf076518e13
Nima sherpa <73970332+NimaSherpa10@users.noreply.github.com>
2024-09-04 08:06:57 -0700

    question 8 solved

---
+++ SevenToEleven/pizza.java
@@ -0,0 +1,23 @@
+import static Utils.Uilities.*;
+
+public class pizza {
+    public static void main(String[] args) {
+        double price = 399.0d;
+            double discount = 20d;
+            String bill = "*" .repeat(50);
+            double discountAmount = percentToAmount(price, discount);
+            double noOfPizza = 2;
+            double finalAmount = noOfPizza * (price + discountAmount);
+            bill += "\n";
+            bill += "\t Pizza Cost Calculator \t\t\n";
+            bill += "\n";
+            bill += "\t Price Amount : \t " + price +  "\n";
+            bill += "\t Discount Percentage : \t " + discount + "% \n";
+            bill +="\t Total Discount Amount :  " + stripDecimalPoints(discountAmount) +  "\t\n";
+            bill += "-".repeat(50);
+            bill += "\n";
+            bill += "\t Final Amount : \t" + " " + stripDecimalPoints(finalAmount) + "\t\n";
+
+            System.out.println(bill);
+    }
+}
