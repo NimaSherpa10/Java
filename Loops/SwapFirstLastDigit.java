2b8c0c66d6ecda82cc9592d20d17ae097d56c4ba
Nima sherpa <73970332+NimaSherpa10@users.noreply.github.com>
2024-09-09 06:46:50 -0700

    swap first and last

--- Loops/SwapFirstLastDigit.java
+++ Loops/SwapFirstLastDigit.java
@@ -16,15 +16,22 @@ public class SwapFirstLastDigit {
         else {
             int last_digit = num % 10;
             int first_digit = 0;
+            int digit_count = 0;
             for(int i = num; i > 0; i /= 10) {
                 if(i < 10) {
                     first_digit = i;
+                   
                 }
+                digit_count ++;
             }
-            System.out.println(last_digit);
-            System.out.println(first_digit);
-            System.out.println("The Swapped numbers are : ")
+            int middle_part = (num % (int) Math.pow(10, digit_count - 1)) / 10;
+            int swapped_num = (last_digit * (int) Math.pow(10, digit_count - 1)) + (middle_part * 10) + first_digit;
+
+            System.out.println("Original number: " + num);
+            System.out.println("Number after swapping first and last digit: " + swapped_num);
+        }
+
+            
         }
     }
-}
 
